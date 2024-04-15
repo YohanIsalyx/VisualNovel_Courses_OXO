9d70b024aec0b3b7894f0f8afc3fb093cf12378b
Debussy <yohan.debusscher@isalyx.games>
2024-04-15 11:07:56 +0200

    Creation classe : Character & PlaceCharacter

---
+++ Assets/Scripts/Place_Character.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Place_Character : MonoBehaviour
+{
+    public int Id;
+    public int PlaceId;
+    public int AnswerCharacterId;
+    public int QuestionCharacterId;
+    public int DialogId;
+}
